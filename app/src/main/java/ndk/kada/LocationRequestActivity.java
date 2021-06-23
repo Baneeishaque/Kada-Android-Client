211b46c0cb1b83d8be9d381049068a2a2cbc0a32
Banee Ishaque K <baneeishaque@gmail.com>
2021-06-23 14:17:59 +0530

    Loc. Req. UI

--- app/src/main/java/ndk/kada/LocationRequestActivity.java
+++ app/src/main/java/ndk/kada/LocationRequestActivity.java
@@ -8,6 +8,7 @@ public class LocationRequestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_request);
     }

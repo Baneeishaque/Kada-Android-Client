0ea273e3424b8c26acb8c3b644c9c6758d79b989
Banee Ishaque K <baneeishaque@gmail.com>
2021-06-23 09:50:43 +0530

    Mobile Number Check, Updated submodule Utils19

--- app/src/main/java/ndk/kada/LoginActivity.java
+++ app/src/main/java/ndk/kada/LoginActivity.java
@@ -1,15 +1,30 @@
 package ndk.kada;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+import ndk.utils_android1.ActivityUtils1;
+import ndk.utils_android14.ActivityWithContexts;
+import ndk.utils_android14.ButtonUtils14;
+import ndk.utils_android16.ValidationUtils16;
+
+public class LoginActivity extends ActivityWithContexts {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButtonUtils14.associateClickAction(currentAppCompatActivity, R.id.buttonSendOtp, v -> {
+
+            EditText editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber);
+            org.javatuples.Pair<Boolean, EditText> validationResult = ValidationUtils16.mobileNumberCheckEditTextWithRequestFocus(org.javatuples.Pair.with(editTextPhoneNumber, "Invalid Phone Number..."));
+            if (validationResult.getValue0()) {
+
+                ActivityUtils1.startActivityForClass(currentActivityContext, LocationDemoActivity.class);
+            }
+        });
     }
 }
